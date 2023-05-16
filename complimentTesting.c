9705769df1bda2705291690cf5fddc5b35bd69c4
RepoWhatYouSow <129552632+RepoWhatYouSow@users.noreply.github.com>
2023-05-15 20:15:51 -0700

    Update complimentTesting.c

--- complimentTesting.c
+++ complimentTesting.c
@@ -1,33 +1,29 @@
-/*
- * Author: Aiden O'Leary
- * Email: aolear01@student.ubc.ca
- * Student Number: 21923214
- * Lab Section: L2G
- * Date: 16/01/2023
- * Purpose:
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "math.h"
 #include <stdint.h>
 
 #define COMPLIMENT_SHIFT 110
 
-int main(void) {
+int twosEncrypt(int num, int bin);
+int twosDecrypt(int num, int bin);
+
+int main(void) 
+{
 	int counter = 0;
     int bin[] = {0,1,0,1,1,0,1,0};
 
     char message[20] = "zzAA..,,??  aaZZttnn";
-	for (int i = 0; i <20; ++i){
+	for (int i = 0; i <20; ++i)
+	{
 		printf("%d ",message[i]);
 	}
 	printf("\n");
     int newNums[20];
-    for (int i = 0; i <20; ++i){
-		newNums[i] = buffer(message[i],bin[counter]);
-		printf("%d ",buffer(message[i],bin[counter]));
+    for (int i = 0; i <20; ++i)
+    {
+	newNums[i] = twosEncrypt(message[i],bin[counter]);
+	printf("%d ",twosEncrypt(message[i],bin[counter]));
         counter = (counter + 1) % 8;
     }
     printf("\n");
@@ -36,13 +32,14 @@ int main(void) {
     counter = 0;
 	printf("\n");
     for (int i = 0; i <20; ++i) {
-        printf("%c ",debuffer(newNums[i],bin[counter]));
+        printf("%c ",twosDecrypt(newNums[i],bin[counter]));
         counter = (counter + 1) % 8;
     }
 
     return 0;
 }
-int buffer(int num, int bin){
+int twosEncrypt(int num, int bin)
+{
     num*=-1;
 
     if (bin == 0)
@@ -53,7 +50,8 @@ int buffer(int num, int bin){
 
 }
 
-int debuffer(int num, int bin){
+int twosDecrypt(int num, int bin)
+{
     num *= -1;
     if(bin == 0)
         return num*=-1;
